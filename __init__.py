ffd1dc02c53231ae908a50210bb55080c11db604
Anthony64M <anthony.mutuku@student.moringaschool.com>
2021-09-12 11:05:08 +0300

    create configs and initialize flask extensions

--- __init__.py
+++ __init__.py
@@ -4,3 +4,12 @@ from config import config_options
 
 bootstrap = Bootstrap()
 
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    # Create the app configs
+    app.config.from_object(config_options[config_name])
+
+    # Initialize flask extension
+    bootstrap.init_app(app)
