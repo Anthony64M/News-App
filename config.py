5de2ab0ded6ac25bd1d591b5a82465730b73223e
Anthony64M <anthony.mutuku@student.moringaschool.com>
2021-09-12 11:51:19 +0300

    Production config child class

--- config.py
+++ config.py
@@ -4,3 +4,14 @@ class Config:
     NEWS_API_BASE_URL ='https://newsapi.org/v1/articles?source={}&apiKey={}'
     NEWS_API_KEY = 'caa505d83e9b49a1b15577dd33860f10'
     SECRET_KEY = 'Anthony64M'
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
