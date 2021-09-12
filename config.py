608170e39a60fdec034fe822f63f946ffa38e34d
Anthony64M <anthony.mutuku@student.moringaschool.com>
2021-09-12 11:50:36 +0300

    Config  the api

--- config.py
+++ config.py
@@ -1,2 +1,6 @@
 import os
 
+class Config:
+    NEWS_API_BASE_URL ='https://newsapi.org/v1/articles?source={}&apiKey={}'
+    NEWS_API_KEY = 'caa505d83e9b49a1b15577dd33860f10'
+    SECRET_KEY = 'Anthony64M'
