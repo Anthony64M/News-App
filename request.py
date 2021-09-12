3d390795374e3d005c66558e8f56915f34cd04a8
Anthony64M <anthony.mutuku@student.moringaschool.com>
2021-09-12 11:26:03 +0300

    Function returns the top headlines from numerous sources

--- request.py
+++ request.py
@@ -6,3 +6,23 @@ from .models import News
 api_key = None
 # Getting the  base url
 base_url = None
+
+
+def configure_request(app):
+    global api_key,base_url
+    api_key = app.config['NEWS_API_KEY']
+    base_url = app.config['NEWS_API_BASE_URL']
+
+
+
+def get_news(headlines):
+    
+    '''
+    Function returns the top headlines from numerous sources
+    '''
+    
+    newsapi =NewsApiClient(api_key)
+
+    headlines = newsapi.get_top_headlines(sources ="")
+
+    return headlines
