b6c2fb188a5ddcabbac8c0eb37343aa89cf8da77
Anthony64M <anthony.mutuku@student.moringaschool.com>
2021-09-12 11:39:21 +0300

    Functions that search results from apikey provided

--- request.py
+++ request.py
@@ -50,4 +50,26 @@ def get_aljazeera_news(headlines):
     headlines = newsapi.get_top_headlines(sources ="al-jazeera-english")
 
     return headlines
-   
+
+
+def search_news(keyword):
+    
+    '''
+    Function returns the top headlines from a search query
+    '''
+    search_news_url = 'https://newsapi.org/v2/everything?q={}&apiKey=caa505d83e9b49a1b15577dd33860f10'.format(keyword)
+    with urllib.request.urlopen(search_news_url) as url:
+        search_news_data = url.read()
+        search_news_response = json.loads(search_news_data)
+
+        search_results = search_news_response
+
+        if search_news_response['articles']:
+            search_news_list = search_news_response['articles']
+            search_movie_results = process_results(search_news_list)
+
+
+
+
+    return search_results
+
