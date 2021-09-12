0b7b32b9a6773d913a5df111c6ebf5c9d318f9c0
Anthony64M <anthony.mutuku@student.moringaschool.com>
2021-09-12 12:02:34 +0300

    Imported rener_template,main,get_abc_news ...

---
+++ app/main/views.py
@@ -0,0 +1,4 @@
+from flask import render_template,request,redirect,url_for
+from flask import render_template,request,redirect,url_for
+from . import main
+from ..request import get_abc_news, get_aljazeera_news, get_bbc_news, get_news, search_news
